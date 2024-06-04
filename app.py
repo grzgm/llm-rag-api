ac0b8bd2fb1317e4d05bfa1617cb6c71d1ec4228
grzgm <g.malisz@student.fontys.nl>
2024-06-04 17:46:36 +0200

    secrets handling

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
+import os
 from flask import Flask
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, World! </p>"
+
+@app.route("/api")
+def api_key():
+    return f"API KEY: ${os.getenv("API_KEY")}"
